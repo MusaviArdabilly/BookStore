66e8ec906f1aa5432fdc20855a24204d18a854ea
Musavi Ardabilly Taufik <201musavi@gmail.com>
2022-04-04 01:57:37 +0700

    Recycle View

--- app/src/main/java/com/example/bookstroreapps/Home.java
+++ app/src/main/java/com/example/bookstroreapps/Home.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,6 +14,7 @@ public class Home extends AppCompatActivity {
 
     TextView tv_welcome;
     Button btn_contactUs;
+    ImageView iv_anime;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,6 +32,14 @@ public class Home extends AppCompatActivity {
             public void onClick(View view) {
                 Intent contactUs = new Intent(Home.this, ContactUs.class);
                 startActivity(contactUs);
+            }
+        });
+
+        iv_anime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent booklistIntent = new Intent(Home.this, BookList.class);
+                startActivity(booklistIntent);
             }
         });
     }
